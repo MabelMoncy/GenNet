c3ed91a17da69b12d1709f8f1588b4f853fcd1bd
MabelMoncy <mabelantomoncy2005@gmail.com>
2025-05-13 18:40:32 +0530

    This is an AI chatbot using llama3.2

---
+++ WikiBot/app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,request,session
+from langchain_community.llms import Ollama
+llm = Ollama(model="llama3.2")
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/ask',methods=['GET','POST'])
+def ask_questions():
+    answers=''
+    if request.method == "POST":
+        try:
+            questions = request.form.get('question')
+            answers = llm.invoke(questions)
+        except:
+            answers = "Unable to grab data..!.Please enter the query again."
+    return render_template('ask.html',answers=answers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
