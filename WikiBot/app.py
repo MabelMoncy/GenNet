d1824578c330fd6aa85d6f61f29a3958c4d4f822
Mabel Anto Moncy <mabelantomoncy2005@gmail.com>
2025-06-11 14:18:24 +0530

    Update app.py

--- WikiBot/app.py
+++ WikiBot/app.py
@@ -12,12 +12,15 @@ def ask_questions():
     answers=''
     if request.method == "POST":
         try:
-            questions = request.form.get('question')
-            answers = llm.invoke(questions)
+            questions = request.form.get('question').lower()
+            if questions == "what is your name":
+                answers = "I am GeNet. Your Ai assistant!"
+            else:
+                answers = llm.invoke(questions)
         except:
             answers = "Unable to grab data..!.Please enter the query again."
     return render_template('ask.html',answers=answers)
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+    
