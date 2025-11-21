24c340736e4ce186ac9bf5f6972fa909e8c1c9fa
MabelMoncy <184001565+MabelMoncy@users.noreply.github.com>
2025-11-21 18:37:32 +0530

    updated model issue

--- WikiBot/app.py
+++ WikiBot/app.py
@@ -4,7 +4,7 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 
 load_dotenv()
-llm = ChatGroq(model="llama-3.1-8b-instan",temperature = 0)
+llm = ChatGroq(model="llama-3.1-8b-instant",temperature = 0)
 app = Flask(__name__)
 
 @app.route('/')
